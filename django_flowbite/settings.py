873346fa44072fc26f1827eae43acac13874b184
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-17 11:16:59 +0700

    fix: update STATICFILES_DIRS to point to staticfiles directory

--- django_flowbite/settings.py
+++ django_flowbite/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 ]
 
 COMPRESS_ROOT = BASE_DIR / 'staticfiles'
