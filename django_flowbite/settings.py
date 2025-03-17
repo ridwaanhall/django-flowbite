a06b09ffc6ce18c01ab2c28db9bc4914ae13b909
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-17 10:57:01 +0700

    refactor: update Django settings to use environment variables and add Vercel configuration

--- django_flowbite/settings.py
+++ django_flowbite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-md0cbiel_#9zr069vwzat31k2tcf2kxd=k-&5-8qx8x=17ak(3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = [
+        '.vercel.app'
+    ]
 
 
 # Application definition
